2fbc90864a1bdebaff6b6c0a47add85a3f839b5c
igorsam48 <jorgesam202748@gmail.com>
2026-06-18 17:59:03 -0300

    commit 01

---
+++ ProjetoTesteGit/src/testes/Teste01.java
@@ -0,0 +1,11 @@
+package testes;
+
+public class Teste01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Iniciando o projeto");
+	}
+
+}
